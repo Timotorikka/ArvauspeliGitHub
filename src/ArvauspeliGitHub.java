54b33bd665c1d46c3759703d6500f0eae675c9d3
Timotorikka <timo.torikka@student.hamk.fi>
2023-09-23 13:05:22 +0300

    Lisättiin koodiin do while looppi jolla pyydetään käyttäjää arvaamaan nimi

--- src/ArvauspeliGitHub.java
+++ src/ArvauspeliGitHub.java
@@ -8,6 +8,17 @@ public class ArvauspeliGitHub {
 		Scanner in = new Scanner(System.in);
 		int arvaukset = 0;
 		String nimi;
+			
+			// Lisätään do while looppi, jolla pyydetään käyttäjää arvaamaan nimi ja toistetaan niin kauan kunnes arvaa oikein
+			do
+			{
+			System.out.println("Arvaa nimi");			
+			nimi = in.nextLine();			
+			if(nimi.equals("Pentti"))
+				System.out.println("Arvasit oikein, paljon onnea!");
+			
+			
+			} while (!nimi.equals("Pentti"));
 
 	} // Mainin loppu
 
