8fa5116a46ace68de8519c9f59c5cf8b45cbfca1
Timotorikka <timo.torikka@student.hamk.fi>
2023-09-23 12:56:04 +0300

    Lisätty koodiin skanneri käyttäjänsyötettä varten sekä laskuri arvauksia varten ja muuttuja nimelle jonka käyttäjä antaa

--- src/ArvauspeliGitHub.java
+++ src/ArvauspeliGitHub.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
 
 public class ArvauspeliGitHub {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// Tuodaan ensin skanneri käyttäjänsyötettä varten sekä laskuri arvauksia varten ja muuttuja nimelle
+		Scanner in = new Scanner(System.in);
+		int arvaukset = 0;
+		String nimi;
 
-	}
+	} // Mainin loppu
 
 }
